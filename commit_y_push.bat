27f29581e71e79ee7bc1112e0cfe6f802c31dedc
Alejandro <cursomeancore@gmail.com>
2020-03-03 17:28:36 +0100

    Agregado ejercio del tema 8 de arrays

--- commit_y_push.bat
+++ commit_y_push.bat
@@ -1,4 +1,4 @@
 git add .
-git commit -m "Agregado ejercio del tema 5 de objetos"
+git commit -m "Agregado ejercio del tema 8 de arrays
 git push -u origin master
 pause
--- main.js
+++ main.js
@@ -44,9 +44,84 @@ const cliente2 = {
 };
 const cliente3 = {
   id: 3,
-  id_gestor: 2,
+  id_gestor: 80,
   usuario: 'cliente3',
   password: 'cliente3',
   correo: 'cliente3@mail.com',
   saldo: 1000
 };
+
+/*
+Ejercicio proyecto: escribe un programa que almacene los objetos creados en el ejercio anterior del proyecto dentro de un array (un array por cada modelo de datos). A continuación, recorre cada uno de los arrays y muestra todas propiedades.
+*/
+
+// agrego los gestores al array de gestores
+const gestores = [];
+gestores.push(gestor1, gestor2, gestor3);
+// console.log(gestores);
+
+// agrego los clientes al array de clientes
+const clientes = [ cliente1, cliente2, cliente3 ];
+// console.log(clientes);
+
+/* 
+esta función recibe como parámetro el id de un gestor y devuelve su nombre de usuario
+*/
+const obtenerGestorUsuario = (id_gestor) => {
+  
+  for(const gestor of gestores) {
+    if (gestor.id === id_gestor) {
+      return gestor.usuario;
+    }
+  }
+  return 'desconocido';
+};
+
+console.log('GESTORES');
+
+for(const gestor of gestores) {
+  console.log(`Id: ${gestor.id}`);  
+  console.log(`Usuario: ${gestor.usuario}`);  
+  console.log(`Password: ${gestor.password}`);  
+  console.log(`Correo: ${gestor.correo}`);  
+  console.log('-----');
+}
+
+console.log('CLIENTES');
+
+for(const cliente of clientes) {
+
+  const usuarioGestor = obtenerGestorUsuario(cliente.id_gestor);
+
+  console.log(`Id: ${cliente.id}`);  
+  console.log(`Id gestor: ${cliente.id_gestor}`);
+  console.log(`Usuario gestor: ${usuarioGestor}`);
+  console.log(`Usuario: ${cliente.usuario}`);
+  console.log(`Password: ${cliente.password}`);
+  console.log(`Correo: ${cliente.correo}`);  
+  console.log('-----');
+}
+
+
+// agregar los mensajes al array de mensajes
+const mensajes = [
+  {
+    id: 1,
+    id_origen: 1,
+    id_destino: 2,
+    texto: 'hola',
+    fecha: new Date()
+  },
+  {
+    id: 2,
+    id_origen: 2,
+    id_destino: 1,
+    texto: 'adiós',
+    fecha: new Date()
+  }
+];
+
+
+
+
+
